f5d8a7999e9f5caab66c7d05b96d66a5c40ef7ab
Nicky Billovits <njbillov@umich.edu>
2021-02-20 00:20:33 -0500

    Changed user skills to be sorted by progress order and then lexographical order.

--- lib/models/user_skills.dart
+++ lib/models/user_skills.dart
@@ -1,3 +1,4 @@
+import 'package:culi/theme.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'user_skills.g.dart';
@@ -9,18 +10,37 @@ class UserSkills {
 
   UserSkills();
 
-  List<Skill> get masteredSkills =>
-      skills?.where((element) => element.progress == 1)?.toList() ?? [];
+  List<Skill> get masteredSkills {
+    final masteredSkillsList = skills?.where((element) => element.progress == 1)?.toList() ?? [];
+    masteredSkillsList.sort((a, b) => a.name.compareTo(b.name));
+    return masteredSkillsList;
+  }
 
-  List<Skill> get inProgressSkills =>
-      skills
-          ?.where((element) =>
-              (element?.progress ?? 0) < 1 && (element?.progress ?? 0) > 0)
-          ?.toList() ??
-      [];
+  List<Skill> get inProgressSkills {
+    final inProgressSkills = skills
+            ?.where((element) =>
+                (element?.progress ?? 0) < 1 && (element?.progress ?? 0) > 0)
+            ?.toList() ??
+        [];
 
-  List<Skill> get newSkills =>
-      skills?.where((element) => element.progress == 0)?.toList() ?? [];
+    inProgressSkills.sort((a, b) {
+      final comp = (b.progress - a.progress).sign.round();
+      if (comp == 0) {
+        return a.name.compareTo(b.name);
+      }
+      return comp;
+    });
+    return inProgressSkills;
+  }
+
+  List<Skill> get newSkills {
+    final newSkillList =
+        skills?.where((element) => element.progress == 0)?.toList() ?? [];
+
+    newSkillList.sort((a, b) => a.name.compareTo(b.name));
+
+    return newSkillList;
+  }
 
   factory UserSkills.fromJson(Map<String, dynamic> json) =>
       _$UserSkillsFromJson(json);
