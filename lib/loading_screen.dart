1ca32b80cc10a86d23ea758be1bf822a868a3c6d
Nicky Billovits <njbillov@umich.edu>
2021-02-20 00:22:56 -0500

    Condensed the app to be in a single MaterialApp and fixed issues with sending feedback.

--- lib/loading_screen.dart
+++ lib/loading_screen.dart
@@ -1,29 +1,16 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:developer' show log;
-import 'dart:io';
 
-import 'package:device_info/device_info.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:rxdart/rxdart.dart';
-import 'package:screenshot/screenshot.dart';
-import 'package:sensors/sensors.dart';
-import 'package:vector_math/vector_math.dart';
 
-import 'feedback.dart';
 import 'models/account.dart';
-import 'models/feedback.dart';
-import 'models/menus.dart';
 import 'navigation_bar_helpers.dart';
 import 'notification_handlers.dart';
 import 'signup_introduction.dart';
 import 'theme.dart';
 import 'utilities.dart';
-
-final BehaviorSubject<String> selectNotificationSubject =
-    BehaviorSubject<String>();
 
 class LoadingScreen extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -62,22 +49,7 @@ class TopLevelInitializer extends StatefulWidget {
 }
 
 class _TopLevelInitializerState extends State<TopLevelInitializer> {
-  bool inScreenshot = false;
-  StreamSubscription _accelerometerStream;
-  DateTime startShakeEventTime = DateTime.fromMillisecondsSinceEpoch(0);
-  DateTime mostRecentShakeEventTime = DateTime.fromMillisecondsSinceEpoch(0);
-  final int timeRequiredForShakeEventInMillis = 300;
-  final int minHoldOverShakeEventInMillis = 50;
   Future<bool> isInitialized;
-
-  Vector3 lowPassValue;
-  final accelerometerUpdateInterval = 1.0 / 100;
-  final double lowPassKernelWidthInSeconds = 1;
-  double shakeDetectionThreshold =
-      2.0 * 9.81; // Multiply by gravity to correct for platform differences
-
-  double lowPassFilterFactor;
-
   void _configureSelectNotificationSubject() {
     selectNotificationSubject.stream.listen((payload) async {
       log(payload);
@@ -85,165 +57,9 @@ class _TopLevelInitializerState extends State<TopLevelInitializer> {
     });
   }
 
-  void _giveFeedback() async {
-    if (inScreenshot) return;
-    inScreenshot = false;
-    final screenshotController =
-        Provider.of<ScreenshotController>(context, listen: false);
-    log('Taking a screenshot at ${ModalRoute.of(context).settings.name}');
-    final image = await screenshotController.capture();
-
-    final account = Provider.of<Account>(context, listen: false);
-    final menu = Provider.of<Menu>(context, listen: false);
-    final menus = Provider.of<Menus>(context, listen: false);
-    final deviceInfoPlugin =
-        Provider.of<DeviceInfoPlugin>(context, listen: false);
-    var deviceInfo = {};
-    if (Platform.isIOS) {
-      var osInfo = await deviceInfoPlugin.iosInfo;
-      deviceInfo = {
-        'iosVersion': osInfo.utsname.version,
-        'machine': osInfo.utsname.machine,
-        'release': osInfo.utsname.release,
-        'sysnode': osInfo.utsname.sysname,
-        'nodenode': osInfo.utsname.nodename,
-        'model': osInfo.model,
-        'systemVersion': osInfo.systemVersion,
-        'system': osInfo.systemName,
-        'name': osInfo.name
-      };
-    } else if (Platform.isAndroid) {
-      var osInfo = await deviceInfoPlugin.androidInfo;
-      deviceInfo = {
-        'type': osInfo.type,
-        'id': osInfo.id,
-        'androidVersion': osInfo.version,
-        'brand': osInfo.brand,
-        'manufacturer': osInfo.manufacturer,
-        'device': osInfo.device
-      };
-    }
-    final stateDump = jsonEncode([
-      account,
-      {'deviceInfo': deviceInfo},
-      menu,
-      menus
-    ]);
-
-    Widget notWorkingButton = CupertinoDialogAction(
-      child: Text("Something Isn't Working"),
-      isDefaultAction: false,
-      onPressed: () async {
-        // final image = await screenshotController.capture();
-        final feedback = AppFeedback(
-            imageFile: image,
-            type: AppFeedbackType.problem,
-            stateDump: stateDump);
-        if (image != null) {
-          //TODO insert code to upload the image
-        }
-        //TODO insert code to schedule a survey notification
-        Utils.changeScreens(
-            context: context,
-            routeName: '/feedback/error',
-            global: true,
-            nextWidget: () => FeedbackScreen(feedback: feedback));
-        // Navigator.of(context, rootNavigator: true).pop();
-      },
-    );
-
-    Widget feedbackButton = CupertinoDialogAction(
-      child: Text("General Feedback"),
-      isDefaultAction: false,
-      onPressed: () async {
-        // final image = await screenshotController.capture();
-        final feedback = AppFeedback(
-            imageFile: image,
-            type: AppFeedbackType.suggestion,
-            stateDump: stateDump);
-        if (image != null) {
-          //TODO insert code to upload the image
-        }
-        //TODO insert code to schedule a survey notification
-        Utils.changeScreens(
-            context: context,
-            routeName: '/feedback/suggestion',
-            global: true,
-            nextWidget: () => FeedbackScreen(feedback: feedback));
-        // Navigator.of(context, rootNavigator: true).pop();
-      },
-    );
-
-    Widget cancel = CupertinoDialogAction(
-      child: Text("Cancel"),
-      isDefaultAction: true,
-      onPressed: () async {
-        if (image != null) {
-          //TODO insert code to upload the image
-        }
-        //TODO insert code to schedule a survey notification
-        Navigator.of(context, rootNavigator: true).pop();
-      },
-    );
-
-    final alert = CupertinoAlertDialog(
-      title: Text('Report a Problem!'),
-      actions: [notWorkingButton, feedbackButton, cancel],
-    );
-
-    showCupertinoDialog(
-      context: context,
-      builder: (context) => alert,
-    ).then((value) => inScreenshot = false);
-
-    // Navigator.of(context, rootNavigator: true)
-    //     .push(MaterialPageRoute<void>(builder: (context) {
-    //   return Scaffold(
-    //     appBar: AppBar(title: Text('Screenshot'),),
-    //     body: Image.file(image),
-    //   );
-    // }
-    // )).then((value) => inScreenshot = false);
-  }
-
   @override
   void initState() {
     super.initState();
-    lowPassFilterFactor =
-        accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
-    shakeDetectionThreshold *= shakeDetectionThreshold;
-    userAccelerometerEvents.listen((event) {
-      // final prevTime = lastUpdate;
-      // final timeDifference = lastUpdate.difference(prevTime).inMilliseconds / 1000.0;
-      final time = DateTime.now();
-      final acceleration = Vector3(event.x, event.y, event.z);
-      lowPassValue ??= acceleration;
-      lowPassValue += (acceleration - lowPassValue) * lowPassFilterFactor;
-      final deltaAcceleration = acceleration - lowPassValue;
-      if (deltaAcceleration.distanceToSquared(Vector3.zero()) >=
-          shakeDetectionThreshold) {
-        // log('Shake event with magnitude ${acceleration.distanceToSquared(Vector3.zero())}detected at time: ${DateTime.now().toIso8601String()}');
-        final previousDuration = mostRecentShakeEventTime
-            .difference(startShakeEventTime)
-            .abs()
-            .inMilliseconds;
-        if (time.difference(mostRecentShakeEventTime).abs().inMilliseconds >=
-            minHoldOverShakeEventInMillis) {
-          mostRecentShakeEventTime = time;
-          startShakeEventTime = time;
-        } else {
-          // log('Shake length: $previousDuration');
-          mostRecentShakeEventTime = time;
-          if (time.difference(startShakeEventTime).abs().inMilliseconds >
-                  timeRequiredForShakeEventInMillis &&
-              previousDuration <= timeRequiredForShakeEventInMillis) {
-            // log('Detected a shake event');
-            _giveFeedback();
-          }
-        }
-      }
-      // log('Time since last reading: ${timeDifference.toStringAsFixed(3)}s values: $event sqrMagnitude: $sqrMagnitude');
-    });
     isInitialized = NotificationHandler.instance
         .initializePlugin()
         .then((value) => initializeNotificationHandler())
@@ -254,8 +70,6 @@ class _TopLevelInitializerState extends State<TopLevelInitializer> {
   @override
   void dispose() {
     super.dispose();
-    log('Disposing');
-    selectNotificationSubject.close();
   }
 
   void initializeNotificationHandler() async {
@@ -277,20 +91,8 @@ class _TopLevelInitializerState extends State<TopLevelInitializer> {
 
   @override
   Widget build(BuildContext context) {
+    log("Loading screen re-rendered");
     return Provider<Future<bool>>.value(
-      value: isInitialized,
-      child: MaterialApp(
-          title: 'Culi',
-          theme: culiTheme,
-          initialRoute: '/loading',
-          routes: {
-            // '/home': (context) => InteractionNavigator(),
-            '/loading': (context) => LoadingScreen(),
-            // '/selection': (context) => InteractionNavigator(),
-            '/introduction': (context) => NewSignupIntroduction(),
-            '/onboarding': (context) => NewSignupIntroduction(),
-            '/createAccount': (context) => SignupForm1(),
-          }),
-    );
+        value: isInitialized, child: LoadingScreen());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
+import 'dart:convert';
 import 'dart:developer';
 import 'dart:io';
 
 import 'package:device_info/device_info.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_badger/flutter_app_badger.dart';
 import 'package:provider/provider.dart';
+import 'package:rxdart/rxdart.dart';
 import 'package:screenshot/screenshot.dart';
+import 'package:sensors/sensors.dart';
 import 'package:timezone/data/latest.dart' as tz;
+import 'package:vector_math/vector_math.dart';
 
 import 'data_manipulation/gql_interface.dart';
+import 'feedback.dart';
 import 'loading_screen.dart';
 import 'models/account.dart';
+import 'models/feedback.dart';
 import 'models/menus.dart';
 import 'theme.dart';
+import 'utilities.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -31,6 +39,9 @@ void main() async {
   );
 }
 
+final BehaviorSubject<String> selectNotificationSubject =
+    BehaviorSubject<String>();
+
 class CuliInteractionNavigator extends StatefulWidget {
   // This widget is the root of your application.
   final Account account;
@@ -46,8 +57,147 @@ class CuliInteractionNavigator extends StatefulWidget {
 }
 
 class _CuliInteractionNavigatorState extends State<CuliInteractionNavigator> {
+  final GlobalKey<NavigatorState> appState = GlobalKey<NavigatorState>();
   ScreenshotController screenshotController;
   DeviceInfoPlugin deviceInfo;
+
+  bool inScreenshot = false;
+  DateTime startShakeEventTime = DateTime.fromMillisecondsSinceEpoch(0);
+  DateTime mostRecentShakeEventTime = DateTime.fromMillisecondsSinceEpoch(0);
+  final int timeRequiredForShakeEventInMillis = 300;
+  final int minHoldOverShakeEventInMillis = 50;
+
+  Vector3 lowPassValue;
+  final accelerometerUpdateInterval = 1.0 / 100;
+  final double lowPassKernelWidthInSeconds = 1;
+  double shakeDetectionThreshold =
+      1.5 * 9.81; // Multiply by gravity to correct for platform differences
+
+  double lowPassFilterFactor;
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    selectNotificationSubject.close();
+  }
+
+  void _giveFeedback() async {
+    if (inScreenshot) return;
+    inScreenshot = false;
+    // log(mounted ? "mounted" : "not mounted");
+    // print(
+    //     appState.currentContext != null ? "appstate exists" : "app state null");
+    // log('Taking a screenshot at ${ModalRoute.of(appState.currentContext).settings.name}');
+    final image = await screenshotController.capture();
+
+    var deviceInfoMap = {};
+    if (Platform.isIOS) {
+      var osInfo = await deviceInfo.iosInfo;
+      deviceInfoMap = {
+        'iosVersion': osInfo.utsname.version,
+        'machine': osInfo.utsname.machine,
+        'release': osInfo.utsname.release,
+        'sysnode': osInfo.utsname.sysname,
+        'nodenode': osInfo.utsname.nodename,
+        'model': osInfo.model,
+        'systemVersion': osInfo.systemVersion,
+        'system': osInfo.systemName,
+        'name': osInfo.name
+      };
+    } else if (Platform.isAndroid) {
+      var osInfo = await deviceInfo.androidInfo;
+      deviceInfoMap = {
+        'type': osInfo.type,
+        'id': osInfo.id,
+        'androidVersion': osInfo.version,
+        'brand': osInfo.brand,
+        'manufacturer': osInfo.manufacturer,
+        'device': osInfo.device
+      };
+    }
+    final stateDump = jsonEncode([
+      widget.account,
+      {'deviceInfo': deviceInfoMap},
+      widget.menu,
+      widget.menus
+    ]);
+
+    Widget notWorkingButton = CupertinoDialogAction(
+      child: Text("Something Isn't Working"),
+      isDefaultAction: false,
+      onPressed: () async {
+        // final image = await screenshotController.capture();
+        final feedback = AppFeedback(
+            imageFile: image,
+            type: AppFeedbackType.problem,
+            stateDump: stateDump);
+        if (image != null) {
+          //TODO insert code to upload the image
+        }
+        //TODO insert code to schedule a survey notification
+        Utils.changeScreens(
+            context: appState.currentContext,
+            routeName: '/feedback/error',
+            global: true,
+            nextWidget: () => FeedbackScreen(feedback: feedback));
+        // Navigator.of(context, rootNavigator: true).pop();
+      },
+    );
+
+    Widget feedbackButton = CupertinoDialogAction(
+      child: Text("General Feedback"),
+      isDefaultAction: false,
+      onPressed: () async {
+        // final image = await screenshotController.capture();
+        final feedback = AppFeedback(
+            imageFile: image,
+            type: AppFeedbackType.suggestion,
+            stateDump: stateDump);
+        if (image != null) {
+          //TODO insert code to upload the image
+        }
+        //TODO insert code to schedule a survey notification
+        Utils.changeScreens(
+            context: appState.currentContext,
+            routeName: '/feedback/suggestion',
+            global: true,
+            nextWidget: () => FeedbackScreen(feedback: feedback));
+        // Navigator.of(context, rootNavigator: true).pop();
+      },
+    );
+
+    Widget cancel = CupertinoDialogAction(
+      child: Text("Cancel"),
+      isDefaultAction: true,
+      onPressed: () async {
+        if (image != null) {
+          //TODO insert code to upload the image
+        }
+        //TODO insert code to schedule a survey notification
+        Navigator.of(appState.currentContext, rootNavigator: true).pop();
+      },
+    );
+
+    final alert = CupertinoAlertDialog(
+      title: Text('Report a Problem!'),
+      actions: [notWorkingButton, feedbackButton, cancel],
+    );
+
+    showCupertinoDialog(
+      context: appState.currentContext,
+      builder: (context) => alert,
+    ).then((value) => inScreenshot = false);
+
+    // Navigator.of(context, rootNavigator: true)
+    //     .push(MaterialPageRoute<void>(builder: (context) {
+    //   return Scaffold(
+    //     appBar: AppBar(title: Text('Screenshot'),),
+    //     body: Image.file(image),
+    //   );
+    // }
+    // )).then((value) => inScreenshot = false);
+  }
 
   @override
   void initState() {
@@ -55,6 +205,41 @@ class _CuliInteractionNavigatorState extends State<CuliInteractionNavigator> {
     super.initState();
     deviceInfo = DeviceInfoPlugin();
     screenshotController = ScreenshotController();
+    lowPassFilterFactor =
+        accelerometerUpdateInterval / lowPassKernelWidthInSeconds;
+    shakeDetectionThreshold *= shakeDetectionThreshold;
+    userAccelerometerEvents.listen((event) {
+      // final prevTime = lastUpdate;
+      // final timeDifference = lastUpdate.difference(prevTime).inMilliseconds / 1000.0;
+      final time = DateTime.now();
+      final acceleration = Vector3(event.x, event.y, event.z);
+      lowPassValue ??= acceleration;
+      lowPassValue += (acceleration - lowPassValue) * lowPassFilterFactor;
+      final deltaAcceleration = acceleration - lowPassValue;
+      if (deltaAcceleration.distanceToSquared(Vector3.zero()) >=
+          shakeDetectionThreshold) {
+        // log('Shake event with magnitude ${acceleration.distanceToSquared(Vector3.zero())}detected at time: ${DateTime.now().toIso8601String()}');
+        final previousDuration = mostRecentShakeEventTime
+            .difference(startShakeEventTime)
+            .abs()
+            .inMilliseconds;
+        if (time.difference(mostRecentShakeEventTime).abs().inMilliseconds >=
+            minHoldOverShakeEventInMillis) {
+          mostRecentShakeEventTime = time;
+          startShakeEventTime = time;
+        } else {
+          // log('Shake length: $previousDuration');
+          mostRecentShakeEventTime = time;
+          if (time.difference(startShakeEventTime).abs().inMilliseconds >
+                  timeRequiredForShakeEventInMillis &&
+              previousDuration <= timeRequiredForShakeEventInMillis) {
+            // log('Detected a shake event');
+            _giveFeedback();
+          }
+        }
+      }
+      // log('Time since last reading: ${timeDifference.toStringAsFixed(3)}s values: $event sqrMagnitude: $sqrMagnitude');
+    });
   }
 
   @override
@@ -75,17 +260,16 @@ class _CuliInteractionNavigatorState extends State<CuliInteractionNavigator> {
             'name': osInfo.name
           }.toString()));
     }
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(value: widget.account),
-        ChangeNotifierProvider.value(value: widget.menu),
-        ChangeNotifierProvider.value(value: widget.menus),
-        Provider.value(value: DeviceInfoPlugin),
-        Provider.value(value: screenshotController)
-      ],
-      child: Screenshot(
-        controller: screenshotController,
+    return Screenshot(
+      controller: screenshotController,
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider.value(value: widget.account),
+          ChangeNotifierProvider.value(value: widget.menu),
+          ChangeNotifierProvider.value(value: widget.menus),
+        ],
         child: MaterialApp(
+          navigatorKey: appState,
           title: 'Culi Interaction Navigator',
           theme: culiTheme,
           home: TopLevelInitializer(),
